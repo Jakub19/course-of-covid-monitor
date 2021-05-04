e5c9bc487695cedc948947a15801ed13ecbc0d40
Hoerrin <patrykd0604@gmail.com>
2021-05-05 01:28:28 +0200

    cleaned up files, navbar and hero added

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import logo from '../images/logos/logo.svg'
+import NavbarProfile from './NavbarProfile'
+import './Navbar.css'
+
+function Navbar(props) {
+    const isLoggedIn = props.isLoggedIn;
+
+    return (
+        <nav className="navbar">
+            <img className="navbar__logo" src={logo} alt="logo" />
+            <div className="navbar__buttons">
+                <button className="navbar__button">FAQ</button>
+                {isLoggedIn
+                    ? <NavbarProfile />
+                    : <><button className="navbar__button">Sign In</button>
+                    <button className="navbar__button navbar__button--register">Register now</button></>
+                }
+            </div>
+
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ client/src/components/NavbarProfile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./NavbarProfile.css"
+
+function NavbarProfile() {
+    return (
+        <div className="navbar__profile--container">
+            <div className="navbar__profile--icon"></div>
+            <div className="navbar__profile--username">Jan Kowalski</div>
+        </div>
+    )
+}
+
+export default NavbarProfile
