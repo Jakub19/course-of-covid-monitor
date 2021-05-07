283081f963136c59d9ac6b81bbf0ad9efb6422ac
Hoerrin <patrykd0604@gmail.com>
2021-05-07 14:50:44 +0200

    Minor changes to navbar animation ang class naming

--- client/src/components/NavbarProfile.js
+++ client/src/components/NavbarProfile.js
@@ -6,10 +6,11 @@ function NavbarProfile() {
     return (
         <nav className="navbarProfile">
             <img className="navbarProfile__logo" src={logo} alt="logo" />
-            <div className="navbarProfile__buttons">
-                <button className="navbarProfile__button" id="btn1"><div className="navbarProfile__button--box"></div><p>Overview</p>  </button>
-                <button className="navbarProfile__button">Settings</button>
-            </div>
+            <ul className="tab">
+            <span className="tab__indicator"></span>
+                <li className="tab__button tab__button--active" id="btn1"><p>Overview</p></li>
+                <li className="tab__button">Settings</li>
+            </ul>
             <div className="navbarProfile__profile--container">
                 <div className="navbarProfile__profile--icon"></div>
                 <div className="navbarProfile__profile--username">Jan Kowalski</div>
