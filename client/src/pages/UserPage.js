2063a459bd1d4186af8a6583f6deec4b0fc6e9a1
Hoerrin <patrykd0604@gmail.com>
2021-05-15 00:16:21 +0200

    Changed background rendering

--- client/src/pages/UserPage.js
+++ client/src/pages/UserPage.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Footer from '../components/Footer'
-import ProfileBackground from '../components/ProfileBackground'
 import ProfileOverview from '../components/ProfileOverview'
 import ProfileNavbar from '../components/ProfileNavbar'
 import ProfileSummary from '../components/ProfileSummary'
@@ -12,7 +11,6 @@ import "../components/UserPage.css"
 function UserPage() {
     return (
         <div className='userpage'>
-            <ProfileBackground />
             <ProfileNavbar />
             <ProfileOverview />
             <ProfileSummary />
