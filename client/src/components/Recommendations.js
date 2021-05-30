d90c15e1681b05fb019b6aa007d729be70e219dc
Hoerrin <patrykd0604@gmail.com>
2021-05-30 14:07:33 +0200

    Added remaining svg's on homepage

--- client/src/components/Recommendations.js
+++ client/src/components/Recommendations.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './Recommendations.css';
 import StatsCard from '../components/StatsCard';
 import {ReactComponent as IconHome} from '../images/icons/home.svg';
-import {ReactComponent as IconSep} from '../images/icons/bed.svg';
+import {ReactComponent as IconBed} from '../images/icons/bed.svg';
+import {ReactComponent as IconHThermometer} from '../images/icons/thermometer.svg';
+import {ReactComponent as IconHands} from '../images/icons/hands.svg';
+import {ReactComponent as IconSign} from '../images/icons/sign.svg';
+import {ReactComponent as IconSprinkler} from '../images/icons/sprinkler.svg';
 
 
 function Recommendations() {
@@ -18,31 +22,31 @@ function Recommendations() {
                     </h1>
                 </div>
                 <div className="recommendations__item">
-                    <IconSep className="recommendations__icon" />
+                    <IconBed className="recommendations__icon" />
                     <h1 className="recommendations__desc">
                         Separate yourself from other people
                     </h1>
                 </div>
                 <div className="recommendations__item">
-                    <IconSep className="recommendations__icon" />
+                    <IconHThermometer className="recommendations__icon" />
                     <h1 className="recommendations__desc">
                         Monitor your symptoms
                     </h1>
                 </div>
                 <div className="recommendations__item">
-                    <IconSep className="recommendations__icon" />
+                    <IconHands className="recommendations__icon" />
                     <h1 className="recommendations__desc">
                         Clean your hands often
                     </h1>
                 </div>
                 <div className="recommendations__item">
-                    <IconSep className="recommendations__icon" />
+                    <IconSign className="recommendations__icon" />
                     <h1 className="recommendations__desc">
                         Avoid sharing household items
                     </h1>
                 </div>
                 <div className="recommendations__item">
-                    <IconSep className="recommendations__icon" />
+                    <IconSprinkler className="recommendations__icon" />
                     <h1 className="recommendations__desc">
                         Clean all “high-touch” surfaces everyday
                     </h1>
