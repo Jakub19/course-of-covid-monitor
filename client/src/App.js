6faa07c9bdf7b6d0572355d89beef7f98d72a57f
Hoerrin <patrykd0604@gmail.com>
2021-06-04 22:32:34 +0200

    Added routing on profile and settings page

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,18 @@ import Homepage from './pages/Homepage';
 import UserPage from './pages/UserPage';
 import Register from './pages/Register';
 import {Switch, Route} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">  
+    <div className="App">
+    <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route path="/profile" component={UserPage} />
           <Route path="/register" component={Register} />
         </Switch>
+    </BrowserRouter>  
     </div>
   );
 }
--- client/src/pages/UserPage.js
+++ client/src/pages/UserPage.js
@@ -3,17 +3,31 @@ import Footer from '../components/Footer'
 import ProfileOverview from '../components/ProfileOverview'
 import ProfileNavbar from '../components/ProfileNavbar'
 import ProfileSettings from '../components/ProfileSettings'
-
+import {
+    Switch,
+    Route,
+    useRouteMatch
+} from "react-router-dom";
+    
 import "./UserPage.css"
 
 
 
 function UserPage() {
+
+    const { path } = useRouteMatch();
+
     return (
         <div className='userpage'>
             <ProfileNavbar />
-            <ProfileOverview />
-            <ProfileSettings /> 
+            <Switch>
+                <Route exact path={path}>
+                <ProfileOverview />
+                    </Route>
+                <Route path={`${path}/settings`}>
+                    <ProfileSettings />
+                </Route>
+            </Switch>
             <Footer />
         </div>
     )
