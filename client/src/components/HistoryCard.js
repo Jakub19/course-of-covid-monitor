36f56b2975edf64b468a7928fb73e33e17ecaf2a
Hoerrin <patrykd0604@gmail.com>
2021-06-13 15:02:02 +0200

    Fixed autofill in register form

--- client/src/components/HistoryCard.js
+++ client/src/components/HistoryCard.js
@@ -8,10 +8,6 @@ function HistoryCard(props) {
             {
                 label: 'Series 1',
                 data: [[0, 1], [1, 2], [2, 4], [3, 2], [4, 7]]
-            },
-            {
-                label: 'Series 2',
-                data: [[0, 3], [1, 1], [2, 5], [3, 6], [4, 4]]
             }
         ],
         []
